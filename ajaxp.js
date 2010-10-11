cdddb2b24483fd795c9e7c83acf7d80fdefe6396
Michael Anderson <michael.anderson@runwithrobots.com>
2010-10-11 13:07:43 +1030

    Added many more functions

---
+++ ajaxp.js
@@ -0,0 +1,31 @@
+///
+/// AJAXP standard functions
+///
+var serialize = function(obj)
+{
+	var str = [];
+	for(var p in obj)
+	{
+		str.push(p + "=" + encodeURIComponent(obj[p]));
+	}
+	return str.join("&");
+}
+
+// Helper to load the xml in a ajaxp style
+// This keeps the function in memory even after the ajax has completed, which is a bit wasteful.
+var ajax_counter=0;
+var ajax_callbacks=[];
+var do_ajax = function(callback, path, data)
+{
+	data.game = game_name;
+	data.path = path;
+	ajax_callbacks[ajax_counter] = callback;
+	data.callback = "ajax_callbacks["+ajax_counter+"]";
+	ajax_counter = ajax_counter + 1;
+	var script = document.createElement('script');
+	script.setAttribute('src', xhr_address+"?"+serialize(data) );
+
+	// load the script
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
+
