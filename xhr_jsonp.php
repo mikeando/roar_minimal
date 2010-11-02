f4a1b61050a3abd7818612e4a665ee0b3fa4e93f
Michael Anderson <michael.anderson@runwithrobots.com>
2010-11-02 22:29:20 +1030

    Added scripting tab for testing server side scripts

--- xhr_jsonp.php
+++ xhr_jsonp.php
@@ -9,7 +9,7 @@ if (!empty($_GET['game']) )
 }
 else
 {
-	define ('HOSTNAME', 'http://roar.io/lpm/');
+	define ('HOSTNAME', 'http://localhost/server/');
 }
 $callback = $_GET['callback'];
 unset($_GET['callback']);
@@ -26,7 +26,25 @@ $session = curl_init($url);
 
 // If it's a POST, put the POST data in the body
 curl_setopt ($session, CURLOPT_POST, true);
-curl_setopt ($session, CURLOPT_POSTFIELDS, $_GET);
+
+$flatget = array();
+foreach( $_GET as $k => $v )
+{
+	if( is_array($v) )
+	{
+		foreach( $v as $kk => $vv )
+		{
+			printf( "// %s[%s] = %s\n", $k, $kk, $vv );
+			$flatget[ $k.'['.$kk.']' ] =$vv;
+		}
+	}
+	else
+	{
+		$flatget[$k]=$v;
+	}
+} 
+
+curl_setopt ($session, CURLOPT_POSTFIELDS, $flatget);
 
 // Don't return HTTP headers. Do return the contents of the call
 curl_setopt($session, CURLOPT_HEADER, false);
